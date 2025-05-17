cf08753587ad53fe58ae5531a9a14975fab2263e
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-05-17 16:32:30 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-const timerElement = document.querySelector('#timer-1');
-const daysElement = timerElement.querySelector('[data-value="days"]');
-const hoursElement = timerElement.querySelector('[data-value="hours"]');
-const minsElement = timerElement.querySelector('[data-value="mins"]');
-const secsElement = timerElement.querySelector('[data-value="secs"]');
+class CountdownTimer{
+    constructor(selector, data){
+        this.timerElem = document.querySelector(selector)
+        this.data = data
+        this.start()
+    }
+    
 
-const targetDate = new Date('Jun 17, 2025');
+    start(){
+        this.update()
+        this.interval = setInterval(() => this.update(), 1000)
+    }
 
-function updateTimer() {
-  const currentTime = new Date();
-  const time = targetDate - currentTime;
-  
-  if (time <= 0) {
-    clearInterval(timerInterval);
-    displayTime(0, 0, 0, 0);
-    return;
-  }
-  
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  
-  displayTime(days, hours, mins, secs);
+    update(){
+        const time = this.data.getTime() - Date.now();
+
+        if(time === 0){
+            clearInterval(this.interval)
+            return
+        }
+
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+
+
+        this.timerElem.querySelector('[data-value="days"]').textContent = days.toString().padStart(2, "0");
+        this.timerElem.querySelector('[data-value="hours"]').textContent = hours.toString().padStart(2, "0");
+        this.timerElem.querySelector('[data-value="mins"]').textContent = mins.toString().padStart(2, "0");
+        this.timerElem.querySelector('[data-value="secs"]').textContent = secs.toString().padStart(2, "0");
+
+    }
 }
 
-function displayTime(days, hours, mins, secs) {
-  daysElement.textContent = days.toString().padStart(2, '0');
-  hoursElement.textContent = hours.toString().padStart(2, '0');
-  minsElement.textContent = mins.toString().padStart(2, '0');
-  secsElement.textContent = secs.toString().padStart(2, '0');
-}
-
-const timerInterval = setInterval(updateTimer, 1000);
-updateTimer();
-
+new CountdownTimer("#timer-1", new Date("Jun 17, 2025"));
